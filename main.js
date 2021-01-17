c4dd2e5c85d3f261003800cf514180054df2a52e
dolenguyetthanh <dolenguyetthanh@gmail.com>
2021-01-17 21:40:20 +0700

    Add aside-left part

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 $("#menu-icon").click(function() {
 	$("#main-menu").css("display", "flex");
+	$("#aside").css("visibility", "hidden");
 });
 $("#main-menu").click(function() {
 	$("#main-menu").css("display", "none");
+	$("#aside").css("visibility", "visible");
 });
